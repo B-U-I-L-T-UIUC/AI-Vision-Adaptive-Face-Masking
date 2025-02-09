64fed6fb45dcc9b828e1ed436a6c098342a6233b
scuruchima1 <scuruchima1@gmail.com>
2025-02-09 17:13:21 -0600

    init files and directories

---
+++ aws-backend/main.tf
@@ -0,0 +1,2 @@
+# main file for our amazon web services (AWS) infrastructure 
+# this backend will contain infrastructure that will handle user requests and send them to our ml-backend for further processing
---
+++ ml-backend/main.py
@@ -0,0 +1,2 @@
+# we will begin to write the ml python face masking application here
+# the program will listen to an mqtt topic for requests, coming from the aws-backend which will be accessed from the react-frontend
