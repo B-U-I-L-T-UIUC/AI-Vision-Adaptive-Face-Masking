991263d663d281e84a5ddf2f685e2b5047309ac2
Steven Uruchima <64672490+scuruchima1@users.noreply.github.com>
2025-02-24 10:29:34 -0600

    Add AWS S3 Remote for Terraform (#39)
    * added terraform state backend on aws s3

--- aws-backend/providers.tf
+++ aws-backend/providers.tf
@@ -5,6 +5,11 @@ terraform {
       version = "~> 5.0"
     }
   }
+  backend "s3" {
+    bucket = "eoh-tf-state"      # S3 bucket where the state file will be stored
+    key    = "terraform.tfstate" # Path within the S3 bucket (e.g., "terraform/state.tfstate")
+    region = "us-east-1"      # AWS region for the S3 bucket
+  }
 }
 
 # Configure the AWS Provider
