7914cfd1369f2e9ae1a917b686316379fc0a7647
jasmin salgado <jsalg20@illinois.edu>
2025-03-19 01:18:51 -0500

    first few changes

--- react_frontend/src/components/avatarGalleryModal.js
+++ react_frontend/src/components/avatarGalleryModal.js
@@ -2,15 +2,14 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles/avatarGalleryModal.css"
 import AddAvatar from "../components/addAvatar";
-const avatars = Array(19).fill({ src: "/assets/avatarExample.png" });
+
+const avatars = Array(10).fill({ src: "/assets/avatarExample.png" });
 
 export default function AvatarGallery() {
   const navigate = useNavigate();
 
   return (
     <div className="avatar-gallery-container">
-      <h1 className="gallery-title">AVAFM</h1>
-
       <div className="avatar-grid-container">
         <div className="avatar-grid">
           <div className="select-avatar-text">SELECT YOUR AVATAR</div>
