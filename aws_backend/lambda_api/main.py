2b97ebb6d548532ce0754c026432b6806a4cda8a
Steven Uruchima <64672490+scuruchima1@users.noreply.github.com>
2025-03-25 08:55:25 -0500

    add api endpoints and lambda api template (#67)

--- aws_backend/lambda_api/main.py
+++ aws_backend/lambda_api/main.py
@@ -2,6 +2,35 @@ import json
 import boto3
 import os
 
+### Needed endpoints ###
+# Image upload 
+#   POST /image/{userId}
+# User avatar feature change
+#   POST /feature/{userId}
+# Get user data from ml backend
+#   GET /user/{userId}
+
+### Message Payload Structure ###
+# This message payload will be sent to and proccessed by the ml backend
+# {
+#     "requestId" : context.aws_request_id,
+#     "userId" : userId,
+#     "requestType" : requestType,
+#     "event" : specifiedEvent,
+# }
+
+### image upload specifiedEvent ###
+# {
+#     "imageURL" : s3ImageUrl,
+# }
+
+### avatar feature change specifiedEvent ###
+# {
+#     "feature" : feature
+#     "{feature}" : featureParam
+# }
+
+
 # Initialize AWS IoT Data client
 iot_client = boto3.client('iot-data', region_name=os.environ.get("AWS_REGION", "us-east-1"))
 
@@ -12,26 +41,48 @@ def lambda_handler(event, context):
     """
     AWS Lambda handler function to publish a message to an MQTT topic in AWS IoT Core.
     """
+
+    resource_path = event["resource"].split("/")
+    method_type = event["httpMethod"]
+
     try:
+
+        if resource_path[1] == "v1":
+            if method_type == "POST" and resource_path[2] == "image":
+                pass
+            if method_type == "POST" and resource_path[2] == "feature":
+                pass
+            if method_type == "GET" and resource_path[2] == "user":
+                pass
+        
         # Construct the message payload
-        message_payload = {
-            "requestId": context.aws_request_id,
-            "color": event["pathParameters"]["color"],
-            "event": event["body"]
-        }
+        # message_payload = {
+        #     "requestId": context.aws_request_id,
+        #     "color": event["pathParameters"]["color"],
+        #     "event": event["body"]
+        # }
 
         # Publish the message to the MQTT topic
-        response = iot_client.publish(
-            topic=MQTT_TOPIC,
-            qos=1,  # QoS Level (0: At most once, 1: At least once)
-            payload=json.dumps(message_payload)
-        )
+        # response = iot_client.publish(
+        #     topic=MQTT_TOPIC,
+        #     qos=1,  # QoS Level (0: At most once, 1: At least once)
+        #     payload=json.dumps(message_payload)
+        # )
 
+        # return {
+        #     "statusCode": 200,
+        #     "body": json.dumps({
+        #         "message": f"Message published to MQTT topic: {MQTT_TOPIC}",
+        #         "response": response
+        #     })
+        # }
+    
         return {
             "statusCode": 200,
             "body": json.dumps({
-                "message": f"Message published to MQTT topic: {MQTT_TOPIC}",
-                "response": response
+                "message": f"successfully hit lambda function",
+                "resource_path": resource_path,
+                "method_type": method_type
             })
         }
 
