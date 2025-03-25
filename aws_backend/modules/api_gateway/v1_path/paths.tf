c527be4ccdb844ca994de804d7e97bc51a461d22
Steven Uruchima <64672490+scuruchima1@users.noreply.github.com>
2025-03-25 08:55:25 -0500

    add api endpoints and lambda api template (#67)

---
+++ aws_backend/modules/api_gateway/v1_path/paths.tf
@@ -0,0 +1,26 @@
+resource "aws_apigatewayv2_integration" "lambda_api" {
+  api_id           = var.api_gw_http_id
+  integration_type = "AWS_PROXY"
+  integration_uri  = var.api_lambda_invoke_arn
+}
+
+# POST /v1/image/{userId}
+resource "aws_apigatewayv2_route" "image_upload" {
+  api_id    = var.api_gw_http_id
+  route_key = "POST /v1/image/{userId}"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_api.id}"
+}
+
+# POST /v1/feature/{userId}
+resource "aws_apigatewayv2_route" "avatar_feature_change" {
+  api_id    = var.api_gw_http_id
+  route_key = "POST /v1/feature/{userId}"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_api.id}"
+}
+
+# GET /v1/user/{userId}
+resource "aws_apigatewayv2_route" "user_data" {
+  api_id    = var.api_gw_http_id
+  route_key = "GET /v1/user/{userId}"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_api.id}"
+}
