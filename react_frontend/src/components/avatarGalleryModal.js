2a2ae65e460ac15201920aa470920a0e6b209a46
jasmin salgado <jsalg20@illinois.edu>
2025-03-26 16:09:31 -0500

    fixed add button positioning

--- react_frontend/src/components/avatarGalleryModal.js
+++ react_frontend/src/components/avatarGalleryModal.js
@@ -19,7 +19,7 @@ export default function AvatarGallery() {
                 <img src={avatar.src} alt="avatar" className="avatar-image" />
               </button>
             ))}
-          <AddAvatar/>
+            <AddAvatar/>
           </div>
         </div>
     </div>
