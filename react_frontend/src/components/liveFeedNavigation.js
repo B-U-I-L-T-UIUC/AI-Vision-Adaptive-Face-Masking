f179bb1c608bb26e8675b08c0c08c907c7f6c41a
paloma888 <palomapichardo@icloud.com>
2025-03-29 15:17:29 -0500

    added resize buttons

--- react_frontend/src/components/liveFeedNavigation.js
+++ react_frontend/src/components/liveFeedNavigation.js
@@ -1,14 +1,12 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import "../styles/liveFeedNavigation.css"
+import NavigatingButtons from "./navigatingButtons";
+import ResizeButtons from "./resizeButtons";
 export default function LiveFeedNavigation() {
-    const navigate = useNavigate();
     return (
         <>
-        <div className="buttons">
-            <img src="/assets/arrow.png" alt="right-arrow" className="left_img"/>
-            <img src="/assets/galleryicon.png" alt="gallery" className="gallery_button" onClick={() => navigate("../avatar-gallery")}/>
-            <img src="/assets/arrow.png" alt="right-arrow" className="right_img"/>
+        <div className="all_buttons">
+            <NavigatingButtons />
+            <ResizeButtons />
         </div>
         </>
     );
